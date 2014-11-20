775be64997bec6f76aaa65acb06103c0a74af3ab
Dylan <dylankulchyski@gmail.com>
2014-11-20 16:44:18 -0600

    created a category controller and some navigation from main page to product

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,2 +1,16 @@
 class ProductController < ApplicationController
+   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @products_in_stock = Product.where("stock_quantity > 0")
+  end
+  
+  def show
+  end
+  
+  
+  private
+    def set_product
+      @product = Product.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
+  resources :product
+  resources :finder
+  resources :category
+  
   root to: 'finder#index'
+  
   
 end
